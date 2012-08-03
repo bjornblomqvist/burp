81f80b1e24aa3334d63ba7385d6e28b13580d234
Darwin <darwin.git@marianna.se>
2012-08-03 11:12:12 +0200

    just a start

--- app/assets/editing/burp/editing.js
+++
@@ -1,8 +0,0 @@
-//
-//= require jquery
-//= require jquery.ui.draggable
-//= require jquery.ui.droppable
-//= require ./editing/js/md5.js
-//= require ./editing/js/showdown.js
-//= require ./editing/js/admin-dock.js
-//= require ./editing/js/content-decorator.js
---
+++ app/assets/packages/burp/editing.js
@@ -0,0 +1,13 @@
+//
+//= require jquery
+//= require jquery.ui.draggable
+//= require jquery.ui.droppable
+//= require ./editing/js/md5.js
+//= require ./editing/js/showdown.js
+//= require ./editing/js/admin-dock.js
+//= require ./editing/js/content-decorator.js
+//= require ./editing/dep/CodeMirror-2.3/lib/codemirror.js
+//= require ./editing/dep/CodeMirror-2.3/mode/javascript/javascript.js
+//= require ./editing/dep/CodeMirror-2.3/mode/htmlmixed/htmlmixed.js
+//= require ./editing/dep/CodeMirror-2.3/mode/xml/xml.js
+//= require ./editing/dep/CodeMirror-2.3/mode/markdown/markdown.js
