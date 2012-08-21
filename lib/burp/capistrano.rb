6c77cd0bca7a6040dba2f7fb615e15fd24423d59
Darwin <darwin.git@marianna.se>
2012-08-21 11:07:02 +0200

    added merge capistrano tasks

---
+++ lib/burp/capistrano.rb
@@ -0,0 +1,37 @@
+
+Capistrano::Configuration.instance.load do
+  namespace :burp do
+
+    desc 'Creates the CMS dir in the shared directory'
+    task :setup do
+      run "mkdir #{shared_path}/cms"
+      run "git init #{shared_path}/cms"
+    end
+
+    desc 'Links the shared CMS dir'  
+    task :link_cms_dir do
+      run "rmdir #{latest_release}/app/cms; ln -s #{shared_path}/cms #{latest_release}/app/cms"
+    end
+    after 'deploy:update', 'deploy:link_cms_dir'
+
+    desc "Merges the live CMS with the local"
+    task :pull, :roles => :web, :once => true do
+  
+      server = roles[:web].servers.first.host
+      user = ssh_options[:user]+"@" || ""
+  
+      `cd app/cms; git pull #{user}#{server}:#{shared_path}/cms/ master`
+    end
+
+    desc "Push local changes to live"
+    task :push, :roles => :web, :once => true do
+      server = roles[:web].servers.first.host
+      user = ssh_options[:user]+"@" || ""
+  
+      `cd app/cms; git push #{user}#{server}:#{shared_path}/cms/ master:local_master`
+      run 'cd #{shared_path}/cms; git merge local_master'
+    end
+
+  end
+end
+    
