47b4bedd5c50eb657cf8e1e13b2b7673ca4b886c
Darwin <darwin.git@marianna.se>
2012-08-21 16:44:52 +0200

    fixed so that when automering fails we undo it and show an error message

--- lib/burp/capistrano.rb
+++ lib/burp/capistrano.rb
@@ -25,31 +25,26 @@ Capistrano::Configuration.instance.load do
 
     desc "Push local changes to live"
     task :push, :roles => :web, :once => true do
+      
       server = roles[:web].servers.first.host
       user = ssh_options[:user]+"@" || ""
-  
+      
       `cd app/cms; git push #{user}#{server}:#{shared_path}/cms/ master:local_master`
-      run "cd #{shared_path}/cms; git merge local_master"
       
-      unmerged_paths = false
-      puts "now"
-      run "cd #{shared_path}/cms; git status" do |channel, stream, data|
-        puts "yes!"
-        puts "#{data}"
-        unmerged_paths = true if data.match(/Unmerged paths/)
+      transaction do
+        on_rollback do
+          run "cd #{shared_path}/cms; git checkout -f"
+          puts "\n\e[0;31m   ######################################################################"
+          puts "   #\n   #                  Auto merge faild."
+          puts "   #\n   #       Do \"cap burp:pull\" resolve the merge and the try again."
+          puts "   #\n"
+          puts "   ######################################################################\e[0m\n"
+        end
+  
+        run "cd #{shared_path}/cms; git merge local_master"
       end
-      puts "now2"
       
-      if unmerged_paths
-        run "cd #{shared_path}/cms; git checkout -f"
-        puts "\n\e[0;31m   ######################################################################"
-        puts "   #\n   #                  Auto merge faild."
-        puts "   #\n   #       Do \"cap burp:pull\" resolve the merge and the try again."
-        puts "   #\n"
-        puts "   ######################################################################\e[0m\n"
-      end
     end
-
   end
 end
     
