d4a928e008394460daaab0ebbd39f4a678fd5fb8
Darwin <darwin.git@marianna.se>
2012-08-21 16:19:20 +0200

    ???

--- lib/burp/capistrano.rb
+++ lib/burp/capistrano.rb
@@ -29,7 +29,25 @@ Capistrano::Configuration.instance.load do
       user = ssh_options[:user]+"@" || ""
   
       `cd app/cms; git push #{user}#{server}:#{shared_path}/cms/ master:local_master`
-      run "cd #{shared_path}/cms; git merge local_master" 
+      run "cd #{shared_path}/cms; git merge local_master"
+      
+      unmerged_paths = false
+      puts "now"
+      run "cd #{shared_path}/cms; git status" do |channel, stream, data|
+        puts "yes!"
+        puts "#{data}"
+        unmerged_paths = true if data.match(/Unmerged paths/)
+      end
+      puts "now2"
+      
+      if unmerged_paths
+        run "cd #{shared_path}/cms; git checkout -f"
+        puts "\n\e[0;31m   ######################################################################"
+        puts "   #\n   #                  Auto merge faild."
+        puts "   #\n   #       Do \"cap burp:pull\" resolve the merge and the try again."
+        puts "   #\n"
+        puts "   ######################################################################\e[0m\n"
+      end
     end
 
   end
