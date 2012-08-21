6a641952bebd50ae287a19c1f10cdc930ed15dc1
Darwin <darwin.git@marianna.se>
2012-08-21 11:29:30 +0200

    trying to get assets to compile

--- lib/burp/engine.rb
+++ lib/burp/engine.rb
@@ -4,5 +4,12 @@ module Burp
     
     config.autoload_paths << File.expand_path("../../../app/lib", __FILE__)
     
+    # Enabling assets precompiling under rails 3.1
+    if Rails.version >= '3.1'
+      initializer :assets do |config|
+        Rails.application.config.assets.precompile += %w( burp/editing.less burp/editing.js )
+      end
+    end
+    
   end
 end
