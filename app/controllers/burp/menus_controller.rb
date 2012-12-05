1117530eb725af4dd0b62d1935f6e2f6641f4921
Darwin <darwin.git@marianna.se>
2012-12-05 11:26:57 +0100

    its a nice feature to see a list of pages not in the menu

--- app/controllers/burp/menus_controller.rb
+++ app/controllers/burp/menus_controller.rb
@@ -8,6 +8,18 @@ module Burp
     def edit
       @menu = Menu.find(params[:id])
       @menu.update_id("")
+      
+      all_items = @menu.all_children
+      
+      @pages_not_in_menu = Group.new("pages not in menu")
+      
+      PageModel.all_paths.each do |path|
+        path2 = path == "/" ? path : path + "/"
+        if(all_items.select{|item| item.is_a?(Link) && (item.url == path || item.url == path2)}.length == 0) 
+          page = PageModel.find(path)
+          @pages_not_in_menu.children << Link.new(page.title => page.path)
+        end
+      end
     end
     
     def update
