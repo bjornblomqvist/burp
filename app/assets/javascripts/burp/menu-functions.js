d41a8761c58c971f10c717111150d8e4220de61a
Darwin <darwin.git@marianna.se>
2012-12-05 11:14:26 +0100

    we where saving the wrong urls to the menu

--- app/assets/javascripts/burp/menu-functions.js
+++ app/assets/javascripts/burp/menu-functions.js
@@ -12,20 +12,12 @@
   
   $.fn.serializeGroup = function() {
     if($(this).is("section.group")) {
-      // Link clanup
-      $(this).find('a').each(function() {
-        if($(this).attr('orginial-url')) {
-          $(this).attr('href',$(this).attr('orginial-url'));
-          $(this).removeAttr('orginial-url');
-        }
-      });
-      
       return {
         name:($(this).find("> .group-name").text()),
         children:$(this).find("> ul.children > li").map(function() {
           if($(this).is(".link")) {
             var a = $(this).find('a');
-            return {name:a.text(), url:a.attr("href")};
+            return {name:a.text(), url:(a.attr("orginial-url") || a.attr("href"))};
           } else if($(this).is(".group")) {
             return $(this).find('> section.group').serializeGroup();
           }
