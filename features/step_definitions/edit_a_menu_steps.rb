1949674c1aed10b4f638c7426b22da4f90be10fa
Darwin <darwin.git@marianna.se>
2012-12-17 23:00:58 +0100

    changes in the navigation had broken the test

--- features/step_definitions/edit_a_menu_steps.rb
+++ features/step_definitions/edit_a_menu_steps.rb
@@ -5,8 +5,7 @@ Given /^I am on the first page of the admin$/ do
 end
 
 When /^I go and add a link to the first menu$/ do
-  click_link "Menus"
-  click_link "main"
+  click_link "Menu"
   click_link "New link"
   fill_in "Name", :with => "A new link"
   fill_in "Url", :with => "http://video.google.com/"
