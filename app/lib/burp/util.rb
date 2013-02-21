5221fa79b765c2679aa1fe0c97bad145d07441ae
Darwin <darwin.git@marianna.se>
2013-02-21 12:23:50 +0100

    .

---
+++ app/lib/burp/util.rb
@@ -0,0 +1,41 @@
+
+require 'fileutils'
+require 'RMagick'
+
+module Burp
+  module Util
+  
+    def self.commit(message = "auto commit")
+      raise "missing git repo in burp cms directory" unless File.exist?("#{Burp.content_directory}/.git")
+      `cd #{Burp.content_directory}; git add .; git commit -a -m "Burp: #{message}"`
+    end
+    
+    def self.create_smaller_images(file_path)
+      
+      upload_directory_path = "#{Burp.content_directory}uploads/"
+      
+      sizes = {:small => [200,300],:medium => [600,900], :large => [1000,1500]}
+      image = Magick::ImageList.new(file_path).first
+      image.auto_orient!
+      sizes.each_pair do |key,value|
+        FileUtils.mkdir_p("#{upload_directory_path}#{key.to_s}")
+        target_path = "#{upload_directory_path}#{key.to_s}/#{File.basename(file_path)}"
+        if value[0] < image.columns # We only downscale
+          image.resize_to_fit(value[0],value[1]).write(target_path)
+        else
+          File.unlink(target_path) if File.exist?(target_path)
+          File.symlink(file_path,target_path)
+        end
+      end
+      image.destroy!
+    end
+    
+    def self.image_size(file_path)
+      image = Magick::ImageList.new(file_path).first
+      "#{image.columns}x#{image.rows}"
+    ensure
+      image.destroy!
+    end
+    
+  end
+end
